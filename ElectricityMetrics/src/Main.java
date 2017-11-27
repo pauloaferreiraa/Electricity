adccd1eb3ab2403ce84bff2009247be0f1bd24e7
p3ssos <tiago.gomez@hotmail.com>
2017-11-27 19:24:21 +0000

    media e desvio do gasto

--- ElectricityMetrics/src/Main.java
+++ ElectricityMetrics/src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        ReadData rd = new ReadData();
+        Map<String,Double> medias = rd.getMediaDia();
+        for(String s : medias.keySet()){
+            System.out.println(s+"   :"+ medias.get(s));
+        }
+
+        System.out.println("-------------------------------");
+        Map<String,Double> desvio = rd.getDesvioDia();
+        for(String s : desvio.keySet()){
+            System.out.println(s+"   :"+ desvio.get(s));
+        }
+        System.out.println("-------------------------------");
+        Map<String,Double> mediaM = rd.getMediaMes();
+        for(String s : mediaM.keySet()){
+            System.out.println(s+"   :"+ mediaM.get(s));
+        }
+
+        System.out.println("-------------------------------");
+        Map<String,Double> desvioM = rd.getDesvioMes();
+        for(String s : desvioM.keySet()){
+            System.out.println(s+"   :"+ desvioM.get(s));
+        }
+    }
 }
---
+++ ElectricityMetrics/src/ReadData.java
@@ -0,0 +1,175 @@
+import java.io.*;
+import java.util.*;
+
+public class ReadData {
+
+    String csvFile = "Test.csv";
+    String cvsSplitBy = ";";
+    BufferedReader br ;
+    Map<String,Double> mediaDia = new TreeMap<String, Double>();
+    Map<String,Double> desvioDia = new TreeMap<String, Double>();
+    Map<String,Double> mediaMes = new TreeMap<String, Double>();
+    Map<String,Double> desvioMes = new TreeMap<String, Double>();
+
+
+    public Map<String,Double> getMediaDia(){
+        if(mediaDia.size()==0){
+            CalcMediaDia();
+            return mediaDia;
+        }else{
+            return mediaDia;
+        }
+    }
+
+    public Map<String,Double> getDesvioDia(){
+        if(desvioDia.size()==0){
+            CalcDesvioDia();
+            return desvioDia;
+        }else{
+            return desvioDia;
+        }
+    }
+
+    public Map<String,Double> getMediaMes(){
+        if(mediaMes.size()==0){
+            CalcMediaMes();
+            return mediaMes;
+        }else{
+            return mediaMes;
+        }
+    }
+
+
+    public Map<String,Double> getDesvioMes(){
+        if(desvioMes.size()==0){
+            CalcDesvioMes();
+            return desvioMes;
+        }else{
+            return desvioMes;
+        }
+    }
+
+
+    public void CalcMediaDia(){
+
+        Map<String, Integer> cont  =new HashMap<String, Integer>();
+
+        try{
+            br= new BufferedReader(new FileReader(csvFile));
+            String line=br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] dados = line.split(cvsSplitBy);
+                if(!mediaDia.containsKey(dados[0])){
+                    mediaDia.put(dados[0],getDouble(dados[1])+getDouble(dados[2]));
+                    cont.put(dados[0],1);
+                }else{
+                    mediaDia.put(dados[0],mediaDia.get(dados[0])+getDouble(dados[1])+getDouble(dados[2]));
+                    cont.put(dados[0],cont.get(dados[0])+1);
+                }
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String s : mediaDia.keySet()) {
+            mediaDia.put(s,mediaDia.get(s)/cont.get(s));
+        }
+
+    }
+
+
+    public void CalcDesvioDia(){
+        Map<String, Integer> cont  =new HashMap<String, Integer>();
+        try{
+            br= new BufferedReader(new FileReader(csvFile));
+            String line=br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] dados = line.split(cvsSplitBy);
+                if(!desvioDia.containsKey(dados[0])){
+                    desvioDia.put(dados[0],Math.pow(getDouble(dados[1])+getDouble(dados[2])-mediaDia.get(dados[0]),2));
+                    cont.put(dados[0],1);
+                }else{
+                    desvioDia.put(dados[0],desvioDia.get(dados[0])+Math.pow(getDouble(dados[1])+getDouble(dados[2])-mediaDia.get(dados[0]),2));
+                    cont.put(dados[0],cont.get(dados[0])+1);
+                }
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String s : desvioDia.keySet()) {
+            desvioDia.put(s,Math.sqrt(desvioDia.get(s)/cont.get(s)));
+        }
+
+    }
+
+
+
+
+    public void CalcMediaMes(){
+        Map<String, Integer> cont  =new HashMap<String, Integer>();
+        String mes;
+        String[] data;
+        for(String s : mediaDia.keySet()){
+            data = s.split("/");
+            mes= data[1]+"/"+data[2];
+            if(!mediaMes.containsKey(mes)){
+                mediaMes.put(mes,mediaDia.get(s));
+                cont.put(mes,1);
+            }else{
+                mediaMes.put(mes,mediaMes.get(mes)+mediaDia.get(s));
+                cont.put(mes,cont.get(mes)+1);
+            }
+        }
+
+        for (String s : mediaMes.keySet()) {
+            mediaMes.put(s,mediaMes.get(s)/cont.get(s));
+        }
+
+    }
+
+    public void CalcDesvioMes(){
+        Map<String, Integer> cont  =new HashMap<String, Integer>();
+        String mes;
+        String[] data;
+        for(String s : mediaDia.keySet()){
+            data = s.split("/");
+            mes=data[1]+"/"+data[2];
+            if(!desvioMes.containsKey(mes)){
+                desvioMes.put(mes,Math.pow(mediaDia.get(s)-mediaMes.get(mes),2));
+                cont.put(mes,1);
+            }else{
+                desvioMes.put(mes,desvioMes.get(mes)+Math.pow(mediaDia.get(s)-mediaMes.get(mes),2));
+                cont.put(mes,cont.get(mes)+1);
+            }
+        }
+
+        for (String s : mediaMes.keySet()) {
+            mediaMes.put(s,Math.sqrt(mediaMes.get(s)/cont.get(s)));
+        }
+    }
+
+
+
+
+    private double getDouble(String s){
+        double d;
+        try{
+            d=Double.parseDouble(s);
+            return d;
+        }catch (NumberFormatException n){
+            return 0;
+        }
+
+    }
+
+
+}
+
+
